a5457a3f0d1c113b3056f5a568f36e2328fefed4
Vesubius <jeffryperezduarte@gmail.com>
2024-09-28 20:21:55 -0600

    Home And Login Screen

--- app/src/main/java/com/vesudev/pedidosapp/screens/HomeScreen.kt
+++ app/src/main/java/com/vesudev/pedidosapp/screens/HomeScreen.kt
@@ -1,17 +1,24 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.vesudev.pedidosapp.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -22,21 +29,31 @@ import com.vesudev.pedidosapp.ui.theme.PedidosAppTheme
 @Composable
 fun HomeScreen(navController: NavController) {
     PedidosAppTheme() {
+
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = (Alignment.CenterHorizontally)
         ) {
+
             Spacer(modifier = Modifier.padding(50.dp))
 
             Image(painter = painterResource(id = R.drawable.logo), contentDescription = "Logo")
 
             Spacer(modifier = Modifier.padding(50.dp))
 
-            Button(onClick = { navController.navigate(route = AppScreens.LoginScreen.route) }) { Text("Iniciar Sesion") }
+            Button(onClick = { navController.navigate(route = AppScreens.LoginScreen.route) }) {
+                Text(
+                    "Iniciar Sesion"
+                )
+            }
 
             Spacer(modifier = Modifier.padding(50.dp))
 
-            Button(onClick = {navController.navigate(route = AppScreens.SignUpScreen.route)}) { Text("Registrarse") }
+            Button(onClick = { navController.navigate(route = AppScreens.SignUpScreen.route) }) {
+                Text(
+                    "Registrarse"
+                )
+            }
 
             Spacer(modifier = Modifier.padding(80.dp))
 
@@ -47,5 +64,7 @@ fun HomeScreen(navController: NavController) {
                 }
             }
         }
+
+
     }
 }
--- app/src/main/java/com/vesudev/pedidosapp/screens/LoginScreen.kt
+++ app/src/main/java/com/vesudev/pedidosapp/screens/LoginScreen.kt
@@ -1,5 +1,7 @@
 package com.vesudev.pedidosapp.screens
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -9,19 +11,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 
 
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 import com.vesudev.pedidosapp.R
 import com.vesudev.pedidosapp.navigation.AppScreens
 import com.vesudev.pedidosapp.ui.theme.PedidosAppTheme
@@ -34,6 +50,12 @@ fun LoginScreen(navController: NavController) {
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            //Variables
+            var email by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+            val context = LocalContext.current
+
+
 
             Spacer(modifier = Modifier.padding(30.dp))
 
@@ -58,20 +80,24 @@ fun LoginScreen(navController: NavController) {
             //Campo correo
             Text(text = "Correo Electronico", fontWeight = FontWeight.Normal, fontSize = 20.sp)
 
-            TextField(
+            OutlinedTextField(
                 modifier = Modifier.padding(top = 10.dp),
-                value = "Correo",
-                onValueChange = {})
+                value = email,
+                onValueChange = { email = it },
+                placeholder = { Text(text = "example@domain.com") })
 
             Spacer(modifier = Modifier.padding(top = 50.dp))
 
             //Campo Contraseña
             Text(text = "Contraseña", fontWeight = FontWeight.Normal, fontSize = 20.sp)
 
-            TextField(
+            OutlinedTextField(
                 modifier = Modifier.padding(top = 10.dp),
-                value = "******",
-                onValueChange = {})
+                value = password,
+                onValueChange = { password = it },
+                placeholder = { Text(text = "******") }
+
+            )
 
             // Texto/Link Para Registrarse
             Row {
@@ -84,7 +110,7 @@ fun LoginScreen(navController: NavController) {
                 Text(
                     modifier = Modifier
                         .padding(top = 5.dp, start = 3.dp)
-                        .clickable { navController.navigate(route = AppScreens.SignUpScreen.route)},
+                        .clickable { navController.navigate(route = AppScreens.SignUpScreen.route) },
                     text = "Registrarte",
                     fontWeight = FontWeight.SemiBold,
                     fontSize = 12.sp,
@@ -96,7 +122,19 @@ fun LoginScreen(navController: NavController) {
             //Boton Iniciar Sesion
             Button(
                 modifier = Modifier.padding(top = 20.dp),
-                onClick = {}) { Text(text = "Iniciar Sesion") }
+                onClick = {
+                    if (email.isNotEmpty() and password.isNotEmpty()) {
+                        iniciarSesion(email, password, context, navController)
+                    } else {
+                        Toast.makeText(context, "Los campos estan vacios", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                }) {
+                Text(
+                    text = "Iniciar Sesion"
+                )
+            }
 
             Spacer(modifier = Modifier.padding(10.dp))
 
@@ -129,7 +167,7 @@ fun LoginScreen(navController: NavController) {
 
 
 
-            Row() {
+            Row {
                 Image(
                     modifier = Modifier.clickable { },
                     painter = painterResource(id = R.drawable.google),
@@ -145,4 +183,33 @@ fun LoginScreen(navController: NavController) {
             }
         }
     }
+}
+
+fun iniciarSesion(
+    email: String,
+    password: String,
+    context: Context,
+    navController: NavController
+) {
+    val auth = Firebase.auth
+
+    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            Toast.makeText(context, "Inicio de Sesion exitoso", Toast.LENGTH_LONG).show()
+            navController.navigate(route = AppScreens.BasicAppScreen.route)
+
+        } else {
+            Toast.makeText(context, "Inicio de Sesion Fallido:${task.exception}", Toast.LENGTH_LONG)
+                .show()
+            navController.navigate(route = AppScreens.LoginScreen.route)
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun loginpreview(){
+    LoginScreen(navController = rememberNavController())
 }
